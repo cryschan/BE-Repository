e53d8c5e94fbd63db9405ea1cbaec70186e65845
tato126 <heechan.log@gmail.com>
2025-11-21 16:56:56 +0900

    refactor: 패키지 구조 개선
    - _global/client → _global/config/client 이동
    - AiClientConfig, RestClientConfig 위치 변경
    - 설정 파일의 명확한 계층 구조 확립

--- src/main/java/io/github/cryschan/berepository/_global/config/client/RestClientConfig.java
+++ src/main/java/io/github/cryschan/berepository/_global/config/client/RestClientConfig.java
@@ -1,4 +1,4 @@
-package io.github.cryschan.berepository._global.client;
+package io.github.cryschan.berepository._global.config.client;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
