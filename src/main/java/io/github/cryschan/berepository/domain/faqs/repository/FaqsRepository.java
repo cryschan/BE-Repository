f1bb4bdd675fb0fe9827b338eef80f5324c94059
meaningGitt <swhj0824@swu.ac.kr>
2025-11-21 17:13:52 +0900

    Repository String에서 Long으로 수정

--- src/main/java/io/github/cryschan/berepository/domain/faqs/repository/FaqsRepository.java
+++ src/main/java/io/github/cryschan/berepository/domain/faqs/repository/FaqsRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface FaqsRepository extends JpaRepository<Faqs, String> {
+public interface FaqsRepository extends JpaRepository<Faqs, Long> {
 
     List<Faqs> findAllByOrderBySortOrderAscCreatedAtAsc();
 }
